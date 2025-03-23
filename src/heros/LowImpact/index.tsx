5fdaa484635aed6edb3786b52810b9c9284bd754
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2025-03-23 11:34:11 +0000

    update: heros

--- src/heros/LowImpact/index.tsx
+++ src/heros/LowImpact/index.tsx
@@ -2,23 +2,10 @@ import React from 'react'
 
 import type { Page } from '@/payload-types'
 
-import RichText from '@/components/RichText'
-
-type LowImpactHeroType =
-  | {
-      children?: React.ReactNode
-      richText?: never
-    }
-  | (Omit<Page['hero'], 'richText'> & {
-      children?: never
-      richText?: Page['hero']['richText']
-    })
-
-export const LowImpactHero: React.FC<LowImpactHeroType> = ({ children, richText }) => {
+export const LowImpactHero: React.FC<Page['hero']> = ({ media, title }) => {
   return (
     <div className="container mt-16">
       <div className="max-w-[48rem]">
-        {children || (richText && <RichText data={richText} enableGutter={false} />)}
       </div>
     </div>
   )
